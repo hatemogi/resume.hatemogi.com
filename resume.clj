5b2d9c32a2d74fda98a0af6721a2b85f1fbf3611
Daehyun Kim <hatemogi@gmail.com>
2024-08-16 10:34:35 +0900

    Rust '충분한 기술'로 상향

--- resume.clj
+++ resume.clj
@@ -43,8 +43,8 @@
    [:article.보유기술
     [:div.컬럼
      [:div.기술셋 [:h3 "우수한 기술"] (li ["Scala" "Clojure" "Haskell" "Elm" "Java"])]
-     [:div.기술셋 [:h3 "충분한 기술"] (li ["TypeScript" "JavaScript" "Kotlin" "PostgreSQL" "Amazon Web Services" "Google Cloud Platform" "Netty" "Docker"])]
-     [:div.기술셋 [:h3 "경험한 기술"] (li ["Rust" "Go" "Python" "C" "C++" "Pascal" "Assembly" "Kubernetes"])]]
+     [:div.기술셋 [:h3 "충분한 기술"] (li ["Rust" "TypeScript" "JavaScript" "Kotlin" "PostgreSQL" "Amazon Web Services" "Google Cloud Platform" "Netty" "Docker"])]
+     [:div.기술셋 [:h3 "경험한 기술"] (li ["Go" "Python" "C" "C++" "Pascal" "Assembly" "Kubernetes"])]]
     [:blockquote "주관적 자기 평가입니다. 당장 업무 환경에서 주변 다른 개발자들에게 설명하며 일할 수 있는 수준은 "
      [:span.강조 "우수한 기술"] ", "
      "스스로 현업 환경에서 제 몫을 하며 활용할 수 있는 " [:span.강조 "충분한 기술"] ", "
