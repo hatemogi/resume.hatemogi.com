4fde2e3d9e0344f7fe9c7a99c5725e095a1cc086
Daehyun Kim <hatemogi@gmail.com>
2024-09-02 10:34:54 +0900

    Serverless 수업 수료 추가

--- resume.clj
+++ resume.clj
@@ -199,7 +199,9 @@
            [:div.고딕 연도 ", " 주제 " "
             (when 수료증 [:a {:href 수료증 :title "수료증"} [:i {:class "fa-solid fa-certificate"}]])]])
         (sort-by :연도 >
-                 [{:연도 2024 :주제 "Accelerated Computer Science Fundamentals"
+                 [{:연도 2024 :주제 "Serverless Architectures on AWS"
+                   :수료증 "https://coursera.org/share/38679086890fe82a32ace78abce827bf"}
+                  {:연도 2024 :주제 "Accelerated Computer Science Fundamentals"
                    :수료증 "https://coursera.org/share/dcfb6f581592810253d991a80b250538"}
                   {:연도 2024 :주제 "Ordered Data Structures"
                    :수료증 "https://coursera.org/share/309a4187546674bee7cf1822a84a5b8a"}
